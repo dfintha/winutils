9b2ed72fc9f04a9b3a518c0bad3e41a4a9a03242
Dénes Fintha <d.fintha@spinsplit.com>
2023-09-14 12:36:28 +0200

    (dbglist) Add dump and clear buttons, and fix scrolling and sizing

--- src/dbglist.c
+++ src/dbglist.c
@@ -4,14 +4,20 @@
 
 static HWND hMainWindow = NULL;
 static HWND hOutputList = NULL;
+static HWND hDumpButton = NULL;
+static HWND hClearButton = NULL;
 static HANDLE hBufferReadyEvent = NULL;
 static HANDLE hDataReadyEvent = NULL;
 static HANDLE hMapping = NULL;
 static BOOL bExiting = FALSE;
 static HFONT hFixedFont = NULL;
 
+static CONST INT iButtonWidth = 80;
+static CONST INT iButtonHeight = 30;
 static CONST INT iProcessIdColLength = 75;
 static CONST INT iTimestampColLength = 225;
+static CONST INT iWindowPadding = 10;
+static CONST INT iFixColLength = iProcessIdColLength + iTimestampColLength;
 
 static HFONT LoadFont(IN LONG lSize, IN LPCSTR lpszName) {
     return CreateFont(
@@ -97,6 +103,28 @@ static LPSTR TimestampToString(IN LPSYSTEMTIME lpTimestamp) {
     return aBuffer;
 }
 
+static VOID ScrollOutputListToEnd(VOID) {
+    SendMessage(
+        hOutputList,
+        LVM_ENSUREVISIBLE,
+        (WPARAM)(ListView_GetItemCount(hOutputList) - 1),
+        FALSE
+    );
+}
+
+static VOID ScrollOutputListToSelectedOrEnd(VOID) {
+    if (ListView_GetSelectedCount(hOutputList) == 0) {
+        ScrollOutputListToEnd();
+    } else {
+        SendMessage(
+            hOutputList,
+            LVM_ENSUREVISIBLE,
+            (WPARAM)(ListView_GetNextItem(hOutputList, -1, LVNI_SELECTED)),
+            FALSE
+        );
+    }
+}
+
 static VOID AddEntryToListView(
     IN HWND hList,
     IN LPVOID lpTimestamp,
@@ -127,6 +155,8 @@ static VOID AddEntryToListView(
     lvItem.mask = LVIF_TEXT | LVIF_STATE;
     lvItem.iSubItem = 2;
     ListView_SetItem(hList, &lvItem);
+
+    ScrollOutputListToEnd();
 }
 
 static DWORD WINAPI ListenerThreadProc(IN LPVOID lpParameter) {
@@ -153,6 +183,66 @@ static DWORD WINAPI ListenerThreadProc(IN LPVOID lpParameter) {
     ExitThread(0);
 }
 
+static VOID ResizeMessageColumn(VOID) {
+    RECT rWindow;
+    CONST INT iFix = iFixColLength + iWindowPadding * 2;
+    CONST INT iScrollWidth = GetSystemMetrics(SM_CXVSCROLL);
+
+    GetClientRect(hMainWindow, &rWindow);
+    ListView_SetColumnWidth(
+        hOutputList,
+        2,
+        rWindow.right - iFix - iScrollWidth
+    );
+}
+
+static DWORD StringLength(IN LPCSTR lpString) {
+    DWORD dwLength = 0;
+    while (*lpString++ != '\0')
+        ++dwLength;
+    return dwLength;
+}
+
+static VOID DumpListContents(VOID) {
+    CONST INT iCount = ListView_GetItemCount(hOutputList);
+    CHAR szFileName[MAX_PATH] = "";
+    CHAR szEntryLine[2048] = "";
+    OPENFILENAMEA ofnDialog;
+    OFSTRUCT ofBuffer;
+    HANDLE hFile;
+    INT i, j;
+
+    ZeroMemory(&ofnDialog, sizeof(OPENFILENAMEA));
+    ofnDialog.lStructSize = sizeof(OPENFILENAMEA);
+    ofnDialog.hwndOwner = hMainWindow;
+    ofnDialog.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+    ofnDialog.lpstrFile = szFileName;
+    ofnDialog.nMaxFile = MAX_PATH;
+    ofnDialog.lpstrTitle = "Dump Entries";
+    ofnDialog.Flags = OFN_EXPLORER | OFN_HIDEREADONLY | OFN_ENABLESIZING;
+    ofnDialog.lpstrDefExt = "txt";
+
+    if (!GetSaveFileNameA(&ofnDialog))
+        return;
+
+    hFile = (HANDLE)(UINT_PTR)(OpenFile(szFileName, &ofBuffer, OF_CREATE));
+    for (i = 0; i < iCount; ++i) {
+        for (j = 0; j < 3; ++j) {
+            ListView_GetItemText(hOutputList, i, j, szEntryLine, 2048 - 1);
+            WriteFile(
+                hFile,
+                szEntryLine,
+                StringLength(szEntryLine),
+                NULL,
+                NULL
+            );
+            WriteFile(hFile, " ", 1, NULL, NULL);
+        }
+        WriteFile(hFile, "\n", 1, NULL, NULL);
+    }
+    CloseHandle(hFile);
+}
+
 static LRESULT CALLBACK MainWindowProc(
     IN HWND hWnd,
     IN UINT uMsg,
@@ -160,28 +250,29 @@ static LRESULT CALLBACK MainWindowProc(
     IN LPARAM lParam
 ) {
     RECT rWindow;
-    static CONST INT iFixColLength = iProcessIdColLength - iTimestampColLength;
 
     switch (uMsg) {
-        case WM_SIZING:
-            rWindow = *((LPRECT) lParam);
-            /* fallthrough */
         case WM_SIZE:
-            GetWindowRect(hMainWindow, &rWindow);
+            GetClientRect(hMainWindow, &rWindow);
             SetWindowPos(
                 hOutputList,
                 NULL,
                 0,
                 0,
-                rWindow.right - rWindow.left,
-                rWindow.bottom - rWindow.top,
-                SWP_NOZORDER
+                rWindow.right - iWindowPadding * 2,
+                rWindow.bottom - iWindowPadding * 3 - iButtonHeight,
+                SWP_NOMOVE | SWP_NOZORDER
             );
-            ListView_SetColumnWidth(
-                hOutputList,
-                2,
-                rWindow.right - rWindow.left - iFixColLength
-            );
+            ResizeMessageColumn();
+            ScrollOutputListToSelectedOrEnd();
+            break;
+
+        case WM_COMMAND:
+            if ((HWND)(lParam) == hClearButton) {
+                ListView_DeleteAllItems(hOutputList);
+            } else if ((HWND)(lParam) == hDumpButton) {
+                DumpListContents();
+            }
             break;
 
         case WM_CLOSE:
@@ -243,10 +334,10 @@ static HWND CreateMainWindow(HINSTANCE hInstance) {
         WC_LISTVIEW,
         "",
         WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL,
-        0,
-        0,
-        iWindowWidth,
-        iWindowHeight,
+        iWindowPadding,
+        iButtonHeight + 2 * iWindowPadding,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
         hWnd,
         NULL,
         (HINSTANCE) GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
@@ -256,6 +347,7 @@ static HWND CreateMainWindow(HINSTANCE hInstance) {
         hOutputList,
         LVS_EX_FLATSB | LVS_EX_GRIDLINES | LVS_EX_FULLROWSELECT
     );
+    FlatSB_EnableScrollBar(hOutputList, SB_VERT, ESB_ENABLE_BOTH);
 
     ZeroMemory(&lcColumn, sizeof(LVCOLUMNA));
     lcColumn.mask = 0;
@@ -279,7 +371,7 @@ static HWND CreateMainWindow(HINSTANCE hInstance) {
 
     lcColumn.mask = LVCF_TEXT | LVCF_WIDTH | LVCF_SUBITEM | LVCF_FMT;
     lcColumn.pszText = "Message";
-    lcColumn.cx = iWindowWidth - iProcessIdColLength - iTimestampColLength;
+    lcColumn.cx = CW_USEDEFAULT;
     lcColumn.fmt = LVCFMT_LEFT;
     lcColumn.iSubItem = 3;
     ListView_InsertColumn(hOutputList, 3, &lcColumn);
@@ -293,6 +385,48 @@ static HWND CreateMainWindow(HINSTANCE hInstance) {
         ListView_GetHeader(hOutputList),
         WM_SETFONT,
         (LPARAM) GetStockObject(DEFAULT_GUI_FONT),
+        TRUE
+    );
+
+    hDumpButton = CreateWindow(
+        WC_BUTTON,
+        "Dump",
+        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        iWindowPadding,
+        iWindowPadding,
+        iButtonWidth,
+        iButtonHeight,
+        hWnd,
+        NULL,
+        (HINSTANCE) GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );
+
+    SendMessage(
+        hDumpButton,
+        WM_SETFONT,
+        (LPARAM)(GetStockObject(DEFAULT_GUI_FONT)),
+        TRUE
+    );
+
+    hClearButton = CreateWindow(
+        WC_BUTTON,
+        "Clear",
+        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        iWindowPadding * 2 + iButtonWidth,
+        iWindowPadding,
+        iButtonWidth,
+        iButtonHeight,
+        hWnd,
+        NULL,
+        (HINSTANCE) GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );
+
+    SendMessage(
+        hClearButton,
+        WM_SETFONT,
+        (LPARAM)(GetStockObject(DEFAULT_GUI_FONT)),
         TRUE
     );
 
